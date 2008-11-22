066b844fd4f2c182113261e6165a49d36c8e2912
Paul Legato <pjlegato@gmail.com>
2008-11-22 03:36:58 -0800

    Add copyright info and license.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,23 @@
+# Ruby-Sesame: a Ruby library to interact with OpenRDF.org's Sesame RDF
+# framework via its REST interface.
+#
+# Copyright (C) 2008 Paul Legato (pjlegato at gmail dot com).
+#
+# This file is part of Ruby-Sesame.
+#
+# Ruby-Sesame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ruby-Sesame is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ruby-Sesame.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'rubygems'
 require 'spec'
 require File.join(File.dirname(__FILE__), *%w[shared_ruby_sesame_spec])
